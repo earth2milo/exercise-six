7bacd6203170fea4024dbd9f1b10f5e4c00bf78f
IDMStudent <idmstudent@10-18-155-147.dynapool.wireless.nyu.edu>
2023-11-28 09:22:29 -0500

    firebase setup + auth

---
+++ src/app/components/fireBaseConfig.js
@@ -0,0 +1,11 @@
+const fireBaseConfig = {
+    apiKey: `${process.env.FIREBASE_API_KEY}`,
+    authDomain: "exercise-six-b5f81.firebaseapp.com",
+    projectId: "exercise-six-b5f81",
+    storageBucket: "exercise-six-b5f81.appspot.com",
+    messagingSenderId: "770191176542",
+    appId: "1:770191176542:web:3d0c506de53f094f2895cf"
+  };
+
+
+  export default fireBaseConfig;
