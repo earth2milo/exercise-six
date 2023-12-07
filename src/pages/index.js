4fa4cf6cc5e14d12bb5a2ae9d757509415776b65
IDMStudent <idmstudent@10-18-236-27.dynapool.wireless.nyu.edu>
2023-12-07 08:28:37 -0500

    final updates before submission

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,22 @@
 
-import {useEffect, useState } from 'react';
+import {useEffect } from 'react';
 import {useRouter} from "next/router";
-import { getAuth } from 'firebase/app';
-import { initializeApp } from 'firebase/app';
-import fireBaseConfig from '@/app/components/fireBaseConfig';
-import Header from "@/app/components/Header"
-import UserProfile from '@/app/components/UserProfile';
+import UserProfileCard from '@/app/components/UserProfileCard';
 
 
-export default function userProfile({ isLoggedIn, userInformation}){ 
-   const router=useRouter();
+export default function UserProfile({ isLoggedIn, userInformation}){ 
+    const router = useRouter();
 
     useEffect(() => {
-      if (!isLoggedIn) router.push("/login")
-},[isLoggedIn]);
+        if (!isLoggedIn) router.push("/login")
+    },[isLoggedIn]);
 
 return(
     <>
-    <main>
-    <h1>User Profile</h1>
-    <UserProfile user={userInformation} />
-    </main>
+        <main>
+            <h1>User Profile</h1>
+            <UserProfileCard user={userInformation} />
+        </main>
     </>
 );
-
 }
